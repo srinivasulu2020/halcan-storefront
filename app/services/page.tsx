1e61cc76a96af7162ffa53dd25a273763300a8bd
srinu <srinuwork2020.gmail.com>
2024-03-30 22:44:21 +0530

    halcan

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,11 +1,11 @@
-
+import {Service} from '../services/services'
 const main=()=>{
   return(
     <>
    <section className="py-16 sm:py-20 ">
   <div className="mx-auto max-w-2xl px-4 text-center sm:px-6 lg:max-w-7xl lg:px-8">
     <div className="flex flex-col gap-4 sm:gap-6">
-      <h1 className="text-4xl font-medium tracking-tight sm:text-5xl lg:text-6xl ">wellcome</h1>
+      <h1 className="text-4xl font-medium tracking-tight sm:text-5xl lg:text-6xl ">Services</h1>
       <p className="text-primary-950/70 dark:text-primary-200/70 mx-auto max-w-xl text-lg sm:text-xl">
         Choose the one that best suits your needs.
        
@@ -13,6 +13,7 @@ const main=()=>{
     </div>
   </div>
 </section>
+<Service />
     </>
   )
 }
---
+++ app/services/services.tsx
@@ -0,0 +1,96 @@
+'use client'
+import { useEffect, useState } from "react";
+export const Service=()=>{
+  const [services,setservices]=useState([])
+  const [open,setopen]=useState(false)
+   const [openid,setopenid]=useState('')
+   console.log(openid)
+  
+    useEffect(()=>{
+    const getservices=()=>{
+         fetch('https://ca-central-1.cdn.hygraph.com/content/cloa97imdf5d301uh6jzu3kyp/master'
+          ,{
+              method: 'POST',
+              headers: {'Content-Type':'application/json'},
+              body: JSON.stringify({
+              query: `
+                query Services {
+                    services {
+                      id
+                    name
+                    description
+                  }
+                }
+              `
+             })
+          })
+          .then((res) => res.json())
+            .then((data) => {
+              setservices(data.data.services);   
+        })
+        };  
+        getservices();  
+   },[])
+    return(
+      <>
+        <section className="py-16 sm:py-20">
+          <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+             <div className="grid gap-12 lg:grid-cols-3 lg:gap-8">
+                <div>
+                  <h2 className="text-3xl font-medium tracking-tight sm:text-4xl">Domains and Departments</h2>
+                </div>
+                <div className="lg:col-span-2">
+                  {services.map((service:any) => (
+                  <button 
+                    type="button" value={openid}
+                    className="group block w-full py-4 text-left transition focus-visible:outline-none"
+                    onClick={()=> {
+                      setopen(!(open))
+                      setopenid(service.id)
+                    }}>
+                    <div className={`group-focus-visible:outline-primary-950 dark:group-focus-visible:outline-primary-700 flex items-center justify-between rounded-3xl group-focus-visible:outline group-focus-visible:outline-2 group-focus-visible:outline-offset-2 px-6 ${(open)&&(service.id===openid)?'bg-slate-200':''}  ${(open)&&(service.id===openid)?'p-4':''}`}>
+                    <div className="grid grid-col gap-6">
+                       <span className="font-medium" > {service.name}</span>
+                          
+                        {
+                           (open)&&(service.id===openid)?<span key={service.id}  onChange={(e:any)=>setopenid(e.target.value)}>{service.description } </span>:null   
+                        }
+                    </div>
+                    <span className="ml-6 flex h-7 items-center ">
+                      {
+                        (open)&&(service.id===openid)?
+                        <svg
+                          className="text-primary-600 dark:text-primary-400 h-6 w-6 rotate-0 transform transition duration-200 ease-in-out"
+                        
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 512 512"
+                          fill="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path d="M112 328l144-144 144 144" />
+                        </svg>
+                        :
+                        <svg
+                          className="text-primary-600 dark:text-primary-400 h-6 w-6 rotate-0 transform transition duration-200 ease-in-out"
+                        
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path d="M11.9997 13.1714L16.9495 8.22168L18.3637 9.63589L11.9997 15.9999L5.63574 9.63589L7.04996 8.22168L11.9997 13.1714Z" />
+                        </svg>
+                      }
+                    </span>
+                   </div>
+                     <hr />
+                  </button>
+                  ))}
+              </div>
+            </div>
+          </div>
+        </section>
+
+      </>
+    )
+    }
